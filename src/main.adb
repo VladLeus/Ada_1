0a7cff65e670066e48f7b34b799be034be226660
VladLeus <57189571+VladLeus@users.noreply.github.com>
2024-03-13 15:30:01 +0200

    Update main.adb
    Fixed some issues

--- src/main.adb
+++ src/main.adb
@@ -3,13 +3,16 @@ with Ada.Text_IO;
 procedure Main is
 
    Can_Stop : Boolean := False;
-   pragma Atomic(Can_Stop);
+   pragma Volatile(Can_Stop);
 
-   Thread_Amount : Natural := 5;
+   Thread_Amount : Natural := 2;
    Sum : Long_Long_Integer := 0;
-
+   pragma Volatile(Sum);
+   
    task type Break_Thread;
-   task type Main_Thread;
+   task type Main_Thread is
+      entry Start(Thread_Id: Integer; Step: Integer);
+      end Main_Thread;
 
    task body Break_Thread is
    begin
@@ -18,18 +21,26 @@ procedure Main is
    end Break_Thread;
 
    task body Main_Thread is
+      My_Id: Integer;
+      My_Step: Integer;
    begin
+      accept Start(Thread_Id: Integer; Step: Integer) do
+         My_Id := Thread_Id;
+         My_Step := Step;
+      end Start;
       loop
          delay 1.0;
          Sum := Sum + 1;
          exit when Can_Stop;
       end loop;
 
-      Ada.Text_IO.Put_Line(Sum'Img);
+      Ada.Text_IO.Put_Line("Thread " & My_Id'Img & " " & "Sum: " & Sum'Img);
    end Main_Thread;
 
    B1 : Break_Thread;
    T : array(1 .. Thread_Amount) of Main_Thread;
 begin
-   null;
+   for I in 1..Thread_Amount loop
+      T(I).Start(Thread_ID => I,Step => I+1);
+   end loop;
 end Main;
